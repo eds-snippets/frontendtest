611d6c04b4d446ee87f7ddf557315a702d701566
Edvardas Damušis <edvardas.damusis@dts-solutions.lt>
2022-08-01 22:12:02 +0100

    Part 2

--- src/Image.tsx
+++ src/Image.tsx
@@ -2,18 +2,63 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from '@emotion/react'
+import { RefObject, useEffect, useMemo, useRef, useState } from 'react'
 
 type ImgElementProps = React.ImgHTMLAttributes<HTMLImageElement>
 
 interface ImageProps extends ImgElementProps {}
 
+/**
+ * useShowImage - hook to get ref element intersection over interaction observer
+ * @param ref any element
+ * @returns showImage state
+ */
+const useShowImage = (ref: RefObject<HTMLElement>) => {
+  const [showImage, setShowImage] = useState(false)
+  const intersectionObserver = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) => setShowImage(entry.isIntersecting)),
+    []
+  )
+
+  useEffect(() => {
+    if (ref.current) {
+      intersectionObserver.observe(ref.current)
+      return () => {
+        intersectionObserver.disconnect()
+      }
+    }
+  }, [intersectionObserver, ref])
+
+  return [showImage]
+}
+
 export default function Image (props: ImageProps): JSX.Element {
+  const ref = useRef(null)
+  const [showImage] = useShowImage(ref)
+  const [isImgLoading, setIsImgLoading] = useState(true)
+
+  // we show image always after it is loaded, no matter if it disappears from the screen
   return (
-    <img
-      {...props}
+    <div
+      ref={ref}
       css={css`
-        max-width: 100%;
+        height: 80vh;
+        margin-bottom: 10px;
+        background-color: lightgrey;
       `}
-    />
+    >
+      {(!isImgLoading || showImage) && (
+        <img
+          {...props}
+          onLoad={() => setIsImgLoading(true)}
+          css={css`
+            width: 100%;
+            height: 80vh;
+            object-fit: cover;
+          `}
+        />
+      )}
+    </div>
   )
 }
